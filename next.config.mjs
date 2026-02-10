51194ddcc412cf24e29f73d1958e7c692080347e
vytautassatas <vytautassatas@gmail.com>
2026-02-10 10:16:38 +0100

    Resolve redirects and sitemap

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  trailingSlash: false,
-  async redirects() {
-    return [
-      {
-        source: "/:path*",
-        has: [{ type: "host", value: "longevity-leak.vercel.app" }],
-        destination: "https://longevityleak.com/:path*",
-        permanent: true
-      },
-      {
-        source: "/:path*",
-        has: [{ type: "host", value: "www.longevityleak.com" }],
-        destination: "https://longevityleak.com/:path*",
-        permanent: true
-      }
-    ];
-  }
+  trailingSlash: false
 };
 
 export default nextConfig;
